ac9c347760b6ee6ca2c79b088755b0ecbf2275cf
kavin-lefty <kavinkumarsksi@gmail.com>
2024-02-25 14:32:11 +0530

    kk services-precious metals

--- olsortz/src/App.js
+++ olsortz/src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from "./components/Nav"
 import Section1 from "./components/Home"
+import { Gold } from './components/serv-gold';
 
 
 function App() {
   return (
     <>
-    <Nav />
-    <Section1/>
+    {/* <Nav />
+    <Section1/> */}
+      <Gold/>
     </>
   );
 }
